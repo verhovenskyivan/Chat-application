212e06825819344e0815c9a6ae8b86d1dc97dc72
verhovenskyivan <verhovenskyivan@gmail.com>
2022-07-02 11:08:34 +0300

    new scrollbar and functions

--- Chat application/MVMM/viewmodel/MainViewModel.cs
+++ Chat application/MVMM/viewmodel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Chat_application.MVMM.Model;
+﻿using Chat_application.Core;
+using Chat_application.MVMM.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,14 +9,48 @@ using System.Threading.Tasks;
 
 namespace Chat_application.MVMM.viewmodel
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
+        public RelayCommand SendCommand { get; set; }
         public ObservableCollection <MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        private ContactModel _selectedContact;
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set 
+            { 
+                _selectedContact = value; 
+            OnPropertyChanged();
+            }
+        }
+
+        public string _message;
+        public string Message 
+        {
+            get { return _message; }
+            set 
+            { 
+                _message = value;
+                OnPropertyChanged();
+            }                 
+        } 
+
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+                Message = "";
+            });
 
             Messages.Add(new MessageModel
             {
